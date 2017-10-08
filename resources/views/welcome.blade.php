d297cc33cda1b28f29c7feb0b776fc147a0a974e
hydrogen11 <davidisback4good@hotmail.com>
2017-10-07 20:52:27 -0400

    Push to heroku for test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                     Alex is cool
                 </div>
 
                 <div class="links">
