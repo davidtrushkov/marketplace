b6b9df8d7a65130826413a11f2c15a03a4b8bbca
hydrogen11 <davidisback4good@hotmail.com>
2017-10-10 21:37:17 -0400

    make database credentials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Marketplace</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -80,6 +80,7 @@
             <div class="content">
                 <div class="title m-b-md">
                      Alex is cool
+                    {{ dd(\App\User::all()) }}
                 </div>
 
                 <div class="links">
