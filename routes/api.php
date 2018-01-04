2649d2a8b8d875003f0cb7a3e54ca1e1837b3fdf
hydrogen11 <davidisback4good@hotmail.com>
2018-01-04 18:52:13 -0500

    commit small changes to test webhook URL

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('stripe/webhook', 'Checkout\WebHooksController@handle');
-
 //Route::get('/files', 'Api\FileController@index');
